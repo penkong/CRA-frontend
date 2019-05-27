7efe9007ddfa36da6b891d8bd446d81a4a0b6c2f
mazdak <nazemi.works@gmail.com>
2019-05-27 08:05:04 -0700

    first init with react pages defined

---
+++ frontend/src/router.js
@@ -0,0 +1,28 @@
+//this is kind of App.js
+import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Header from './components/Header';
+import HomePage from './pages/HomePage';
+import PortfolioPage from './pages/PortfolioPage';
+import PortfolioItemPage from './pages/PortfolioItemPage';
+import ContactPage from './pages/ContactPage';
+import NotFoundPage from './pages/NotFoundPage';
+
+const AppRouter = () => (
+  <BrowserRouter>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" component={HomePage} exact={true} />
+        <Route path="/portfolio" component={PortfolioPage} exact={true} />
+        <Route path="/portfolio/:id" component={PortfolioItemPage} />
+        <Route path="/contact" component={ContactPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
+
+export default AppRouter;
