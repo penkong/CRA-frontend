182e3b7debcc152ca52b89426d24a1a9347bd5e5
mazdak <nazemi.works@gmail.com>
2019-05-28 23:21:45 -0700

    almost main done now structure main

---
+++ frontend/src/components/MainInfo/InfoSection.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const InfoSection = () => {
+  return (
+    <p className=" lh-copy measure f4 mt0">
+      Hi,I am <strong className="inlinemv2">Mazdak</strong>  self-thought JavaScript web developer, designer, moving toward fullstack and in love with machine learning.<br/> enjoy to help you.
+      <br/>
+    </p>
+  )
+}
+
+export default InfoSection;
--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -1,6 +1,7 @@
 
 import React from 'react'
-import Button from '../components/Button';
+import Button from '../components/MainInfo/Button';
+import InfoSection from '../components/MainInfo/InfoSection';
 
 const MainPage = () => {
   return (
@@ -8,25 +9,25 @@ const MainPage = () => {
     <div className="cf" style={{}}>
       <div className="fl w-50-l pa3 pa4-ns bg-white black-70 measure-narrow measure-wide-l f3 times br-10">
         <header className="bb b--black-10 pv2">
-        <h3 className="f2 fw6 pa2 mt0 mb1 tc tracked lh-title avenir">Mazdak Nazemi</h3>
+        <h3 className="f2 fw6 pa2 mt0 mb1 tc tracked lh-title code">MAZDAK NAZEMI</h3>
         </header>
         <section className="code pt4 pb3">
-          <p className=" lh-copy measure f4 mt0">
-          Hi,I am <strong className="inlinemv2">Mazdak</strong>  self-thought JavaScript web developer, designer, moving toward fullstack and in love with machine learning.<br/> enjoy to help you.
-          <br/>
-          </p>
-          <h1 className="f5 fw6 ttu tracked mv3">Skills Till Now:</h1>
+          <InfoSection/>
+          <h1 className="f5 fw6 ttu tracked mv3">Skill-set Till Today:</h1>
           <Button text='CorelDraw' color='bg-black'/>
           <Button text='HTML5' color='bg-red'/>
           <Button text='CSS3' color='bg-orange'/>
           <Button text='SASS' color='bg-green'/>
           <Button text='JavaScript' color='bg-navy'/>
+          <Button text='Git' color='bg-purple'/>
           <Button text='Webpack' color='bg-light-blue'/>
           <Button text='ReactJs' color='bg-navy'/>
           <Button text='Redux' color='bg-navy'/>
           <Button text='MongoDB' color='bg-dark-green'/>
           <Button text='Nodejs' color='bg-dark-blue'/>
           <Button text='Express' color='bg-dark-blue'/>
+          <Button text='GraphQl' color='bg-dark-blue'/>
+          <Button text='SSR' color='bg-light-red'/>
           <Button text='OOP & FP' color='bg-gold'/>
         </section>
       </div>
