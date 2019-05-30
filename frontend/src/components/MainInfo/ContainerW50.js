c83fa8bfa81268ff66246f9caf1081f5b8b12f8d
mazdak <nazemi.works@gmail.com>
2019-05-29 22:40:04 -0700

    design almost done

--- frontend/src/components/MainInfo/ContainerW50.js
+++ frontend/src/components/MainInfo/ContainerW50.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ContainerW50 = props => {
   return (
-    <div className={`${props.float} ma1 mt6-l w-70-m w-48-l tc-m pa3 pa4-ns bg-white black-80 measure-narrow measure-l f3`}>
+    <div className={`${props.float} w-70-m w-48-l center ma1 mt4-l tc-m pa3 pa4-ns bg-white black-80 measure-narrow measure-l f3`}>
       <header className="bb b--black-10 pv2">
       <h3 className="f2 fw6 pa2 mt0 mb1 tc tracked lh-title code">{props.head}</h3>
       </header>
