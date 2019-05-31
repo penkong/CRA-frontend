e2df735786717a0840c1b04f3d0997c463bd48c7
mazdak <nazemi.works@gmail.com>
2019-05-31 08:56:47 -0700

    for heroku 2

---
+++ frontend/build/precache-manifest.1c52cecec31ceecf451a7b9474aa4eb7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8439c368d951a3db39be510fd8c40369",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a034678ffa46e9d2802",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "5e54a7d20fc01e242ae4",
+    "url": "/static/css/main.a61a2651.chunk.css"
+  },
+  {
+    "revision": "8a034678ffa46e9d2802",
+    "url": "/static/js/2.ec2cddd8.chunk.js"
+  },
+  {
+    "revision": "5e54a7d20fc01e242ae4",
+    "url": "/static/js/main.a415d6f7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b418247a16dddc008eb09a4c8d9f2554",
+    "url": "/static/media/cat.b418247a.jpg"
+  },
+  {
+    "revision": "3c2319a13c6b51c167b5a6a13a1fb6a4",
+    "url": "/static/media/mlgo.3c2319a1.png"
+  }
+]);
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 
 app.get('/', function (req,res) {
-  res.sendFile(path.resolve(__dirname,'frontend/build/index.html'));
+  res.sendFile(path.join('frontend','build','index.html'));
 })
 
 
