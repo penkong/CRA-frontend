e9e42c17892b984fb4a6784863fc6b6a47f748f3
mazdak <nazemi.works@gmail.com>
2019-05-31 09:05:28 -0700

    for heroku 3

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
+//server.js
+
 const express = require('express');
+const favicon = require('express-favicon');
 const path = require('path');
+const port = process.env.PORT || 8080;
 const app = express();
+app.use(favicon(__dirname + 'frontend/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname,'frontend', 'build')));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname,'frontend', 'build', 'index.html'));
+});
 
-app.get('/', function (req,res) {
-  res.sendFile(path.join('frontend','build','index.html'));
-})
-
-
-
-
-
-
-
-
-const PORT = process.env.PORT || 5001;
-app.listen(PORT);
+app.listen(port);
